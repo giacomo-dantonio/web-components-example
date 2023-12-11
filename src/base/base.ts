b74ce7cb1204b137cef377f1144321dde071877d
Giacomo d'Antonio <giacomo.dantonio@synera.io>
2023-12-11 08:58:39 +0100

    Remove commented out code

--- src/base/base.ts
+++ src/base/base.ts
@@ -3,11 +3,8 @@ import template from './base.html?raw';
 class BaseLayout extends HTMLElement {
   constructor() {
     super();
-    // const template = (document.getElementById("base") as HTMLTemplateElement)?.content;
-
     const shadowRoot = this.attachShadow({ mode: "open" });
     shadowRoot.innerHTML = template;
-    // shadowRoot.appendChild(template.cloneNode(true));
   }
 }
 
