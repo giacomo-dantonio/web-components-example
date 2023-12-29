ab68e369537e0c5527d42d2c374dcb280951f31b
Giacomo d'Antonio <giacomo.dantonio@gmail.com>
2023-12-29 13:32:22 +0100

    Implement start game form

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,3 @@
 import './style.css'
 import './base/base.ts'
+import './start-game/start-game.ts'
--- src/start-game/start-game.ts
+++ src/start-game/start-game.ts
@@ -1,10 +1,30 @@
-import template from './start-game.html?raw';
+import template from './start-game.html?raw'
 
 class StartGame extends HTMLElement {
   constructor() {
-    super();
-    const shadowRoot = this.attachShadow({ mode: "open" });
-    shadowRoot.innerHTML = template;
+    super()
+  }
+
+  connectedCallback() {
+    const shadowRoot = this.attachShadow({ mode: 'open' })
+    shadowRoot.innerHTML = template
+
+    this.updateCategories(shadowRoot)
+  }
+
+  async updateCategories(shadowRoot: ShadowRoot): Promise<void> {
+    const response = await fetch('https://opentdb.com/api_category.php')
+    const json = await response.json()
+    const categories = json['trivia_categories']
+
+    const select = shadowRoot.querySelector("select#category")
+    for (const category of categories) {
+      const option = document.createElement('option')
+      option.setAttribute('value', category['id'])
+      option.textContent = category['name']
+
+      select?.appendChild(option)
+    }
   }
 }
 
